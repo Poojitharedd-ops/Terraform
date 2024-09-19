0019df940ab244d952d451f39ecf021135e537a0
poojitha <poojithadspr@gmail.com>
2024-09-19 10:24:05 +0530

    variables for teraform

---
+++ Ec2/data-sources/data.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "joindevops" {
+  most_recent      = true
+  owners = ["654654279041"]
+  
+
+  filter {
+    name   = "name"
+    values = ["RHEL-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
